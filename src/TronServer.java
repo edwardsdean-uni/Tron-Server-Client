2a57459fb66030db70598297c0413a7dc5aded9d
edwardsdean <edwardsdean@hotmail.com>
2017-11-03 17:58:53 +1000

    finalise basic networking, add sleep function

--- src/TronServer.java
+++ src/TronServer.java
@@ -12,14 +12,19 @@ public class TronServer {
         try {
             Network network = new Network(INET_ADDR, PORT);
 
+            //broadcast ip to already running clients
+            network.broadcast(network.getIP() + "," + network.getPort());
+
+
 
             List<String> players = new ArrayList<>();
 
             //wait for players
             int number_of_players = get_number_of_player();
             for (int i = 0; i < number_of_players; ++i) {
-                network.broadcast(network.getIP() + "," + network.getPort());
-                String command = network.read();
+
+                String command = network.read();  //will wait till recieves a message
+                System.out.println("cammnad " + command);
 
                 if (command.startsWith("add player")) {
                     String[] tokens = command.split(" ");
@@ -37,10 +42,22 @@ public class TronServer {
                 }
             }
 
+            //game
+
+
             } catch(IOException e){
                 e.printStackTrace();
             }
     }
+
+    public static void sleep(int millis){
+        try {
+            Thread.sleep(millis);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
 
 
     public static int get_number_of_player() {
@@ -51,26 +68,3 @@ public class TronServer {
         TronServer.number_of_player = number_of_player;
     }
 }
-//
-//    public static void main(String[] args) throws UnknownHostException, InterruptedException {
-//        // Get the address that we are going to connect to.
-//        InetAddress addr = InetAddress.getByName(INET_ADDR);
-//        // Open a new DatagramSocket, which will be used to send the data.
-//        try (DatagramSocket serverSocket = new DatagramSocket()) {
-//            for (int i = 0; i < 5; i++) {
-//                String msg = "Sent message no " + i;
-//                // Create a packet that will contain the data
-//                // (in the form of bytes) and send it.
-//                DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(),
-//                        msg.getBytes().length, addr, PORT);
-//                serverSocket.send(msgPacket);
-//
-//                System.out.println("Server sent packet with msg: " + msg);
-//
-//                Thread.sleep(500);
-//            }
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
-//    }
-//}
